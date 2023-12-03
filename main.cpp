e8096077c9c19dd566f990f56f02d2649158af2b
Shafi Swapnil <swapnil.env@gmail.com>
2023-12-04 00:56:51 +0600

    configured GCC 20 compiler

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+// #include <iostream>
+
+// int main()
+// {
+//   std::cout << "Hello World!";
+//   return 0;
+// }
+
 #include <iostream>
 
 int main()
 {
-  std::cout << "Hello World!";
-  return 0;
+  auto result = (10 <=> 20) > 0;
+  std::cout << result << std::endl;
 }
