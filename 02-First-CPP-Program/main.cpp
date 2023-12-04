923b231884d036b711a93ce43eedafcc94ecfb46
Shafi Swapnil <swapnil.env@gmail.com>
2023-12-04 20:15:54 +0600

    second cpp program

--- 02-First-CPP-Program/main.cpp
+++ 02-First-CPP-Program/main.cpp
@@ -2,7 +2,25 @@
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
+  // std::cout << "Hello World!" << std::endl;
+
+  // std::cout << "Number1" << std::endl;
+  // std::cout << "Number2" << std::endl;
+  // std::cout << "Number3" << std::endl;
+
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+  std::cout << "Shafi Swapnil" << std::endl;
+
+  // This is one line comment
+  /* This is a multiline comment */
 
   return 0;
 }
